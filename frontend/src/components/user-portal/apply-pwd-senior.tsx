cde7135a2ea7fcf477f6d387ddcf39955a08313c
rence111 <rencemillares619@gmail.com>
2026-09-16 16:06:29 +0800

    fix(portal): keep formatAppDate import in apply-pwd-senior

--- frontend/src/components/user-portal/apply-pwd-senior.tsx
+++ frontend/src/components/user-portal/apply-pwd-senior.tsx
@@ -10,6 +10,7 @@ import { useLanguage } from "../ui/language-context"
 import { getCurrentUserProfile, getLoggedInUserQcid } from "../../utils/userProfile"
 import { subscribeToRealtimeChanges } from "../../utils/realtimeSync"
 import { fetchPwdSeniorApplications } from "../../utils/cachedApiFetch"
+import { formatAppDate } from "./my-applications"
 
 export default function ApplyPWDSenior() {
   const { t, language } = useLanguage()
